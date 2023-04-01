8a4399a8528436b409012e7e4ccfc0bdf415f6fb
Samurai-way <olegmoishevych@gmail.com>
2023-04-01 14:16:31 +0300

    feat: install SwaggerModule

--- src/commons/createApp.ts
+++ src/commons/createApp.ts
@@ -7,8 +7,17 @@ import {
 import { AppModule } from '../app.module';
 import { HttpExceptionFilter } from './exceptionFilter';
 import { useContainer } from 'class-validator';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 export const createApp = (app: INestApplication): INestApplication => {
+  const config = new DocumentBuilder()
+    .setTitle('Instagram api')
+    .setDescription('Instagram api')
+    .setVersion('1.0.0')
+    // .addTag('')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('/api/docs', app, document);
   app.setGlobalPrefix('api');
   app.enableCors();
   useContainer(app.select(AppModule), { fallbackOnErrors: true });
