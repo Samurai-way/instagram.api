ae5f2db75180b3b521f8817ae252173e1eefa8fc
Artur <ghostmatt36@gmail.com>
2023-04-02 22:34:52 +0300

    fix: merge conflict

--- src/app.module.ts
+++ src/app.module.ts
@@ -12,12 +12,19 @@ import { EmailService } from './modules/email/email.service';
 import { EmailRepository } from './modules/email/email.repository';
 import { UsersController } from './modules/users/users.controller';
 import { MailerModule } from '@nestjs-modules/mailer';
+import { AuthService } from './modules/auth/auth.service';
+import { GoogleStrategy } from './modules/auth/google/strategy/google.strategy';
 
 const useCases = [RegistrationUseCase];
-const services = [AppService, PrismaService, EmailService];
+const services = [
+  AppService,
+  PrismaService,
+  EmailService,
+  AuthService,
+  GoogleStrategy,
+];
 const repositories = [AuthRepository, UsersRepository, EmailRepository];
 const controllers = [AppController, AuthController, UsersController];
-import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
@@ -34,7 +41,6 @@ import { AuthModule } from './auth/auth.module';
         },
       },
     }),
-    AuthModule,
   ],
   controllers,
   providers: [...useCases, ...services, ...repositories],
