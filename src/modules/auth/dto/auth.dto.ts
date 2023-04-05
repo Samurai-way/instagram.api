7203e7dabf04f42f1971f083cfa3dc72fd5bfa08
Samurai-way <olegmoishevych@gmail.com>
2023-04-05 19:23:57 +0300

    feat: AuthDto

--- src/modules/auth/dto/auth.dto.ts
+++ src/modules/auth/dto/auth.dto.ts
@@ -1,14 +1,35 @@
 import { Transform, TransformFnParams } from 'class-transformer';
 import { IsEmail, IsString, Length } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class AuthDto {
   @Transform(({ value }: TransformFnParams) => value?.trim())
+  @ApiProperty({
+    description: 'User login',
+    example: 'John',
+    type: 'string',
+    minLength: 3,
+    maxLength: 10,
+  })
   @Length(3, 10)
   login: string;
   @Transform(({ value }: TransformFnParams) => value?.trim())
+  @ApiProperty({
+    description: 'User password',
+    example: 'string',
+    type: 'string',
+    minLength: 6,
+    maxLength: 20,
+  })
   @Length(6, 20)
   password: string;
   @Transform(({ value }: TransformFnParams) => value?.trim())
+  @ApiProperty({
+    description: 'User email',
+    example: 'user@example.com',
+    type: 'string',
+    format: 'email',
+  })
   @IsEmail()
   email: string;
 }
