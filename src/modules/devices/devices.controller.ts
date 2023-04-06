92c68875e66ae849090f3b9018a5e3be0c762933
Samurai-way <olegmoishevych@gmail.com>
2023-04-06 18:14:01 +0300

    feat: getAllDevices docs

--- src/modules/devices/devices.controller.ts
+++ src/modules/devices/devices.controller.ts
@@ -5,12 +5,32 @@ import { GetAlldevicesCommand } from './use-cases/getAllDevices.use-case';
 import { DeleteAlldevicesCommand } from './use-cases/deleteAlldevicesUseCase';
 import { DeleteAllDevicesByDeviceIdCommand } from './use-cases/deleteAllDevicesByDeviceIdUseCase';
 import { Devices } from '@prisma/client';
+import {
+  ApiOperation,
+  ApiResponse,
+  ApiTags,
+  ApiUnauthorizedResponse,
+} from '@nestjs/swagger';
+import { deviceViewModelExample } from '../../../swagger/auth/Device/device=view=model-example';
 
+@ApiTags('SecurityDevices')
 @Controller('security')
 export class DevicesController {
   constructor(public command: CommandBus) {}
 
   @Get('/devices')
+  @ApiOperation({
+    summary: 'Returns all devices with active sessions for current user',
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'Success',
+    schema: { example: [deviceViewModelExample] },
+  })
+  @ApiUnauthorizedResponse({
+    description:
+      'If the JWT refreshToken inside cookie is missing, expired or incorrect',
+  })
   async getAllDevices(@Cookies() cookies): Promise<Devices[]> {
     return this.command.execute(new GetAlldevicesCommand(cookies.refreshToken));
   }
