090ada01959f94bc81ad1569b815475d6a707827
Artur <ghostmatt36@gmail.com>
2023-04-06 13:30:50 +0300

    fix: change swagger doc

--- src/commons/createApp.ts
+++ src/commons/createApp.ts
@@ -15,7 +15,19 @@ export const createApp = (app: INestApplication): INestApplication => {
     .setTitle('Instagram api')
     .setDescription('Instagram api')
     .setVersion('1.0')
+    .addBearerAuth(
+      {
+        type: 'http',
+        scheme: 'bearer',
+        bearerFormat: 'JWT',
+        name: 'JWT',
+        description: 'Enter JWT token',
+        in: 'header',
+      },
+      'JWT-auth',
+    )
     .build();
+
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('/api/docs', app, document);
   app.setGlobalPrefix('api');
