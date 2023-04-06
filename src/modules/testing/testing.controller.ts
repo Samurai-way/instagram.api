72a4f2f567f4736484c462791134b20cef55b33c
Samurai-way <olegmoishevych@gmail.com>
2023-04-06 18:24:19 +0300

    feat: testing all data clear

--- src/modules/testing/testing.controller.ts
+++ src/modules/testing/testing.controller.ts
@@ -1,11 +1,20 @@
 import { Controller, Delete, HttpCode } from '@nestjs/common';
 import { PrismaService } from '../../prisma/prisma.service';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('Clear data of DB')
 @Controller('testing')
 export class TestingController {
   constructor(private readonly prisma: PrismaService) {}
 
   @Delete('/all-data')
+  @ApiOperation({
+    summary: 'Clear all data of db',
+  })
+  @ApiResponse({
+    status: 204,
+    description: 'DB data clear',
+  })
   @HttpCode(204)
   async deleteAllData() {
     return this.prisma.user.deleteMany();
