519359468ecfb56eb0cf66a5f9f6b248617af244
Samurai-way <olegmoishevych@gmail.com>
2023-04-16 10:40:37 +0300

    feat: fix bugs

--- src/modules/adapters/AWS/S3.service.ts
+++ src/modules/adapters/AWS/S3.service.ts
@@ -3,15 +3,16 @@ import { S3 } from 'aws-sdk';
 import { ConfigService } from '@nestjs/config';
 import { v4 as uuid } from 'uuid';
 import { UploadFileCommand } from '../../users/use-cases/upload-file.use-case';
+import * as process from 'process';
 
 @Injectable()
 export class S3Service {
   constructor(private configService: ConfigService) {}
 
-  bucketName = this.configService.get('inctagram-nest');
+  bucketName = this.configService.get(process.env.AWS_BUCKET_NAME);
   s3 = new S3({
-    accessKeyId: this.configService.get('ACCESS_ID'),
-    secretAccessKey: this.configService.get('AWS_SECRET_KEY'),
+    accessKeyId: this.configService.get(process.env.AWS_ACCESS_KEY_ID),
+    secretAccessKey: this.configService.get(process.env.AWS_SECRET_ACCESS_KEY),
   });
 
   async uploadFile(file: Express.Multer.File | UploadFileCommand) {
