e176e1d0b8b92dbbefa98af4be0b127ca8915796
Samurai-way <olegmoishevych@gmail.com>
2023-04-22 22:36:43 +0300

    feat: create ApiUpdateProfileSwagger and other swagger docs for app

---
+++ swagger/User/api-update-profile.ts
@@ -0,0 +1,33 @@
+import { applyDecorators } from '@nestjs/common';
+import {
+  ApiBadRequestResponse,
+  ApiBody,
+  ApiOperation,
+  ApiResponse,
+  ApiUnauthorizedResponse,
+} from '@nestjs/swagger';
+import { UserProfileDto } from '../../src/modules/users/dto/user-profile-dto';
+import { userProfile } from './user-profile';
+import { BadRequestApi } from '../auth/bad-request-schema-example';
+
+export function ApiUpdateProfileSwagger() {
+  return applyDecorators(
+    ApiOperation({ summary: 'Update current user profile' }),
+    ApiBody({
+      description: 'Example request body (all fields not required)',
+      type: UserProfileDto,
+    }),
+    ApiResponse({
+      status: 200,
+      description: 'Returns updated profile',
+      schema: { example: userProfile },
+    }),
+    ApiUnauthorizedResponse({
+      description: 'Unauthorized',
+    }),
+    ApiBadRequestResponse({
+      description: 'If the inputModel has incorrect values',
+      schema: BadRequestApi,
+    }),
+  );
+}
